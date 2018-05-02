78e180cc4e54e6b4ad07db204d2fc8b114d1e772
Rizal muhamad H <rizalmuhamadh@gmail.com>
2018-05-02 18:17:29 +0700

    create navigation menu

--- src/pages/feed/feed.ts
+++ src/pages/feed/feed.ts
@@ -1,8 +1,10 @@
+import { HomePage } from './../home/home';
 import { News } from './../../app/models/news';
 import { NetworkProvider } from './../../providers/network/network';
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
-import { ContentPage } from '../content/content';
+import { IonicPage, NavController, NavParams, ToastController, Platform, MenuController} from 'ionic-angular';
+// import { ContentPage } from '../content/content';
+import { ReadPage } from '../read/read';
 
 /**
  * Generated class for the FeedPage page.
@@ -24,17 +26,38 @@ export class FeedPage {
   data: any;
   dataArray: Array<News>;
 
+  // public unregisterBackButtonAction: any;
+
   url: string;
 
   constructor(public navCtrl: NavController, 
     public navParams: NavParams, 
     private networkProvider: NetworkProvider, 
-    private toast: ToastController) {
-    this.item = navParams.get('item');
-    this.url = networkProvider.url;
+    private toast: ToastController, 
+    public platform: Platform, 
+    menu: MenuController) {
+
+    // this.platform.ready().then(() => {
+    //   this.platform.registerBackButtonAction(() => {
+    //     let nav = app.getActiveNav();
+    //     if(nav.canGoBack()){
+    //       nav.pop();
+    //     }
+        
+    //   });
+    // });
+
+      // let back = platform.registerBackButtonAction(() => {
+      //   this.navCtrl.pop();
+      //   back();
+      // },100);
+      menu.enable(true);
+      this.item = navParams.get('item');
+      this.url = networkProvider.url;
   }
 
   ionViewDidLoad() {
+    // this.initializeBackButtonCustomHandler();
     this.getAllNews();
   }
 
@@ -55,9 +78,24 @@ export class FeedPage {
   }
 
   itemSelected(item){
-    this.navCtrl.push(ContentPage, {
+    this.navCtrl.push(ReadPage, {
       item: item
     });
   }
+
+  gotoBack(){
+    this.navCtrl.setRoot(HomePage);
+  }
+
+  // initializeBackButtonCustomHandler(): void {
+  //   this.unregisterBackButtonAction = this.platform.registerBackButtonAction(function (event) {
+  //     console.log('Prevent Back Button Page Change');
+  //   }, 101); // Priority 101 will override back button handling (we set in app.component.ts) as it is bigger then priority 100 configured in app.component.ts file */
+  // }  
+
+  // ionViewWillLeave() {
+  //   // Unregister the custom back button action for this page
+  //   this.unregisterBackButtonAction && this.unregisterBackButtonAction();
+  // }
 
 }
