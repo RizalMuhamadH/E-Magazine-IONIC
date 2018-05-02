8c356a65e2cbf13ed0fb865179719222b6abe786
Rizal muhamad H <rizalmuhamadh@gmail.com>
2018-05-02 18:16:28 +0700

    back stack

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
+import { HomePage } from './../pages/home/home';
 import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Platform, App } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
@@ -10,12 +11,31 @@ import { HomePage } from '../pages/home/home';
 export class MyApp {
   rootPage:any = HomePage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, app: App) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+    });
+
+    platform.registerBackButtonAction(() =>{
+      console.log('platform');
+      
+      let nav = app.getActiveNavs()[0];
+      let activeView = nav.getActive();
+
+      if(activeView.name == 'FeedPage'){
+        nav.setRoot(HomePage);
+      } else {
+        if (nav.canGoBack()) {
+          console.log('back');
+          nav.pop();
+        } else {
+          console.log('exit');
+          platform.exitApp();
+        }
+      }
     });
   }
 }
